aff723db2eccc8ea35b548f53bf86afd57a0668d
macbookpro-wrx <@ziyiyuwang.sohu-inc.com>
2023-08-10 16:28:20 +0800

    Initial commit

--- sync/case/once.go
+++ sync/case/once.go
@@ -1,1 +1,40 @@
 package _case
+
+import (
+	"fmt"
+	"sync"
+)
+
+type onceMap struct {
+	sync.Once
+	data map[string]int
+}
+
+func (m *onceMap) LoadData() {
+	m.Do(func() {
+		list := []string{"A", "B", "C", "D"}
+		for _, item := range list {
+			_, ok := m.data[item]
+			if !ok {
+				m.data[item] = 0
+			}
+			m.data[item] += 1
+		}
+	})
+}
+
+func OnceCase() {
+	o := &onceMap{
+		data: make(map[string]int),
+	}
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			o.LoadData()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(o.data)
+}
