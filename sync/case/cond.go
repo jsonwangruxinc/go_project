a72126e35842284e23f112fa6ce41c025a861d47
macbookpro-wrx <@ziyiyuwang.sohu-inc.com>
2023-08-15 11:12:38 +0800

    Initial commit

--- sync/case/cond.go
+++ sync/case/cond.go
@@ -35,3 +35,51 @@ func ReadList(list *[]int, c *sync.Cond) {
 	}
 	fmt.Println("list数据为：", *list)
 }
+
+type queue struct {
+	list []int
+	code *sync.Cond
+}
+
+func NewQueue() *queue {
+	q := &queue{
+		list: []int{},
+		code: sync.NewCond(&sync.Mutex{}),
+	}
+	return q
+}
+func (q *queue) Put(item int) {
+	q.code.L.Lock()
+	defer q.code.L.Unlock()
+	q.list = append(q.list, item)
+	//	当数据写入成功后，唤醒一个协程来处理数据
+	q.code.Signal()
+}
+func (q *queue) GetMany(n int) []int {
+	q.code.L.Lock()
+	defer q.code.L.Unlock()
+	for len(q.list) < n {
+		q.code.Wait()
+	}
+	list := q.list[:n]
+	q.list = q.list[n:]
+	return list
+}
+func CodeQueueCase() {
+	//	初始化一个队列
+	q := NewQueue()
+	//	声明一个WaitGroup
+	var wg sync.WaitGroup
+	for n := 1; n <= 10; n++ {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			lsit := q.GetMany(n)
+			fmt.Printf("%d:%d \n", n, lsit)
+		}(n)
+	}
+	for i := 0; i < 100; i++ {
+		q.Put(i)
+	}
+	wg.Wait()
+}
